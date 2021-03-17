f48e5f7ade94311fba1652a11bdbca2e71f5de09
Carl <lundgren.e.carl@gmail.com>
2021-03-17 16:02:03 -0700

    commit tree f75b8435d304616a20df6f80350deb0e22a49dba

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(start) {
+    if (start >= 42){
+        return (start-42);
+    } else {
+        return (42-start);
+    }
+}
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start)*264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start >= end){
+        return (start-end)*264;
+    } else {
+        return (end-start)*264;
+    }
+}
+
+function calculatesFarePrice(start, end) {
+    if (distanceTravelledInFeet(start, end) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, end)>400 && distanceTravelledInFeet(start, end)<=2000) {
+        return (distanceTravelledInFeet(start, end) - 400)*.02;
+    } else if (distanceTravelledInFeet(start, end) > 2500) {
+        return 'cannot travel that far';
+    } else {
+        return 25;
+    }
+}
